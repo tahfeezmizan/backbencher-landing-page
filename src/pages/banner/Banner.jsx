69e04dcbe0e158bbc3eccbc8244f33efd521be95
tahfeezmizan <mizanahmed5316@gmail.com>
2025-02-26 20:38:32 +0600

    update index.html title and add Banner component

---
+++ src/pages/banner/Banner.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import bgImg from "../../assets/banner-slider-img.png"
+
+export default function Banner() {
+    return (
+        <div className='' style={}>
+            <div className=""></div>
+        </div>
+    )
+}
